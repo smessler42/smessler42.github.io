439684d19feab01d4c262dc2561db4005ec6bd16
smessler42 <104022846+smessler42@users.noreply.github.com>
2024-03-08 15:01:09 -0500

    Add files via upload

---
+++ alpertenterprisesjavascript.js
@@ -0,0 +1,16 @@
+function calculate() {
+const tables = parseInt(document.getElementById('tables').value);
+const spotters = parseInt(document.getElementById('spotters').value);
+let tpers = tables/spotters;
+let tpersrem = tables%spotters;
+let result = '';
+
+if (tables % spotters !== 0){
+    result += tpersrem + " spotters will have " + (Math.floor(tpers) +1) + " tables.";
+    result += (spotters - tpersrem) + " spotters will have " + (Math.floor(tpers)) + " tables.";
+
+} else {
+    result = tpers + " tables per spotter.";
+}
+document.getElementById('result').innerHTML = result;
+}
